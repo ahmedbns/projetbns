2519c5b5c1d36c23559105ca98f33c8c6a9c54e9
ahmedbns <ahmeed.bns@gmail.com>
2019-01-01 21:31:54 +0100

    ahmed ben salem web service

--- src/AppBundle/Controller/LivreController.php
+++ src/AppBundle/Controller/LivreController.php
@@ -22,7 +22,88 @@ class LivreController extends FOSRestController
     {
     $result = $this->getDoctrine()->getRepository(Livre::class)->findAll();
     if ($result === null) 
-        return new View("there are no tasks", Response::HTTP_NOT_FOUND);
+        return new View("aucun livre", Response::HTTP_NOT_FOUND);
     return $result;
+    }
+
+    /**
+    * @Rest\Post("/books")
+    */
+    public function ajouterlivre(Request $request)
+    {
+        $data = new Livre();
+        $data->setTitre('ajouterlivre');
+        $data->setDescriptif('ahmed ben salem');
+        $data->setISBN('dev Crea');
+        $data->setDateedition(new \DateTime());
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($data);
+        $em->flush();
+        return new View("livre est ajouter", Response::HTTP_CREATED);
+    }
+
+    /**
+    * @Rest\Put("/books/{id}")
+    */
+    public function modfierlivre(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository(Livre::class);
+        $data = $repo->find($id);
+        if($data == null)
+        {
+            return new View("NULL VALUES ARE NOT ALLOWED", Response::HTTP_NOT_CREATED);
+        }
+        $data->setTitre('modfier titre');
+        $data->setDescriptif('modifier descriptif');
+        $data->setISBN('modifier isbns');
+        $data->setDateedition(new \DateTime());
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($data);
+        $em->flush();
+        return new View("livre est modifier", Response::HTTP_CREATED);
+    }
+
+    /**
+    * @Rest\Get("/book/{titre}")
+    */
+    public function listeparametrer($titre)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository(Livre::class);
+        $result = $repo->findOneByTitre($titre);
+        if ($result === null) 
+            {return new View("aucun livre", Response::HTTP_NOT_FOUND);}
+        return $result;
+    }
+
+    /**
+    * @Rest\Get("/books/{id}")
+    */
+    public function listelivre($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository(Livre::class);
+        $result = $repo->find($id);
+        if ($result === null) 
+            {return new View("aucun livre", Response::HTTP_NOT_FOUND);}
+        return $result;
+    }
+
+    /**
+    * @Rest\DELETE("/books/{id}")
+    */
+    public function supprimerlivre(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository(Livre::class);
+        $data = $repo->find($id);
+        if($data == null)
+        {
+            return new View("NULL VALUES ARE NOT ALLOWED", Response::HTTP_NOT_CREATED);
+        }
+        $em->remove($data);
+        $em->flush();
+        return new View("livre supprimer", Response::HTTP_CREATED);
     }
 }
